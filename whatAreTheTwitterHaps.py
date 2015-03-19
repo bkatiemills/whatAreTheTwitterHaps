4a8a38ac4b1f14be15958b7f21eb24a28f262b63
BillMills <mills.wj@gmail.com>
2015-03-18 23:11:13 -0700

    smartened up function factorization a little

--- whatAreTheTwitterHaps.py
+++ whatAreTheTwitterHaps.py
@@ -1,6 +1,7 @@
 import twitter
 import datetime
 import dateutil.parser
+import ast
 
 # Consumer keys and access tokens, used for OAuth
 consumer_key = 'kittens'
@@ -43,36 +44,49 @@ def linksOnly(tweets):
 
     return linkTweets
 
-def whatAreTheTwitterHaps(hashtag):
+def whatAreTheTwitterHaps(text, maxCalls=10, maxDays=7):
     '''
-    search for tweets from the last 7 days containing #<hashtag>
-    dump any that don't have an interesting link
-    sort what's left by number of retweets
-    save in a file <hashtag>YYYY-MM-DD, where the date is today's,
-    with tweet id, text, no. retweets and timestamp separated by ' ^^^ '
+    search for unique tweets containing <text>
+    give up once we start finding tweets <maxDays> old
+    don't make more than <maxCalls> to the API
     '''
 
-    f = open(hashtag+str(datetime.date.today()), 'w')
-
-    rawTweets = api.GetSearch(term='#'+hashtag+' +exclude:retweets', count=100)
+    tweets = api.GetSearch(term=text+' +exclude:retweets', count=100)
 
     limiter=0 #don't spam the api
-    while ( (not olderThan(rawTweets[-1], 7)) and limiter<10 ):
-        oldestID = rawTweets[-1].id
-        more = nextPage('#'+hashtag+' +exclude:retweets', oldestID)
+    while ( (not olderThan(tweets[-1], maxDays)) and limiter<maxCalls ):
+        oldestID = tweets[-1].id
+        more = nextPage(text+' +exclude:retweets', oldestID)
         if (len(more)==0):
             break;
-        rawTweets = rawTweets + more
+        tweets = tweets + more
         limiter = limiter+1
 
-    linkedTweets = linksOnly(rawTweets)
+    return tweets
 
-    sortedTweets = sorted(linkedTweets, key=lambda thisone: thisone.retweet_count, reverse=True)
+def sortTweets(tweets, key):
+    '''
+    sorts <tweets> by <key>, and returns the sorted list
+    watch out! I'm a horrible person, and eval'ed this
+    '''
+    hackySort = 'sorted(tweets, key=lambda thisone: thisone.'+key+', reverse=True)'
+    sortedTweets = eval(hackySort)
 
-    for tweet in sortedTweets:
+    return sortedTweets
+
+def logTweets(tweets, prefix='tweets'):
+    '''
+    save <tweets> in a file <prefix>.YYYY-MM-DD, where the date is today's,
+    with tweet id, text, no. retweets and timestamp separated by ' ^^^ '
+    '''
+    f = open(prefix+'.'+str(datetime.date.today()), 'w')
+
+    for tweet in tweets:
         s =  str(tweet.id) + ' ^^^ ' + tweet.text.replace('\n', '') + ' ^^^ ' + str(tweet.retweet_count) + ' ^^^ ' + '"' + tweet.created_at + '"' + '\n'
         s = s.encode('ascii', 'ignore')
         f.write(s)
 
-
-whatAreTheTwitterHaps('openscience')
+tweets = whatAreTheTwitterHaps('#openscience')
+interestingT = linksOnly(tweets)
+sortedT = sortTweets(interestingT, 'retweet_count')
+logTweets(sortedT)
